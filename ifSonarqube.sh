a46872db2da9c9d17c35fd8021177c8396c5ed2d
jamesz2011 <jamesz2011@126.com>
2018-04-12 08:52:35 +0800

    Update ifSonarqube.sh
    update

--- ifSonarqube.sh
+++ ifSonarqube.sh
@@ -3,11 +3,6 @@
 
 #----------------------------------------------------------
 
-
-
-
-
-
 #判断是否存在Sonarqube环境是否启动
 docker ps | grep sonarqube  postgres &> /dev/null
 #如果没有启动，使用docker-compose启动相关的sonarqube容器
@@ -16,15 +11,13 @@ then
 	echo "sonarqube is not up,we will start up it!!!"
    wget https://github.com/jamesz2011/sonarqube/raw/master/postgres_sonarqube.yml
 
-  docker-compose -f postgres_sonarqube.yml up -d
+  #docker-compose -f postgres_sonarqube.yml up -d
+  docker-compose -f postgres_sonarqube.yml up 
     
 
 else
 	echo "Sonarqube is up!!!"
 fi
-
-
-
 
 echo "---------------------------------------------------------"
 
